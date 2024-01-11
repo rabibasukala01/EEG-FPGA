849a7197b6df34a693e2dc9c581366ff235e1e02
Rabi Basukala <ibarbasukala77@gmail.com>
2024-01-11 12:53:09 +0545

    first commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# specify the file name
+filename = 'data/srijan/srijanDatasN.npy'
+# Load the array using numpy
+loaded_arr = np.load(filename)
+# Flatten the data
+loaded_arr= [individual for sublist in loaded_arr for individual in sublist]
+len(loaded_arr)
+sampling_rate=1000000 #in Hz
+fft_result = np.fft.fft(loaded_arr)  #'loaded_arr' is your saved voltage data
+n = len(loaded_arr)
+frequencies = np.fft.fftfreq(n, d=1/sampling_rate)
+plt.plot(frequencies, np.abs(fft_result),color='r')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Magnitude')
+
+# Identify peaks using, for example, the find_peaks function from scipy
+from scipy.signal import find_peaks
+peaks, _ = find_peaks(np.abs(fft_result))
+peak_frequencies = frequencies[peaks]
+
+plt.show()
+
